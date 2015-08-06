47e079c6c4bf2e408789827454821a4ed01c46ff
brettgrigsby <gbrett@gmail.com>
2015-08-06 16:16:40 -0600

    recipe index stylz

---
+++ app/controllers/pantry_ingredients_controller.rb
@@ -0,0 +1,22 @@
+class PantryIngredientsController < ApplicationController
+
+  def new
+    @pantry_ingredient = PantryIngredient.new
+  end
+
+  def create
+    pantry_ingredient = PantryIngredient.new(pantry_ingredient_params)
+    pantry_ingredient.pantry = pantry
+    if pantry_ingredient.save
+      redirect_to new_pantry_ingredient_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def pantry_ingredient_params
+    params.require(:pantry_ingredient).permit(:ingredient_id, :quantity)
+  end
+end
